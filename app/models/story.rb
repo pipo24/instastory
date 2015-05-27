9b2fd8403395423fdcf047bdd1d5afdb78be4e60
pipo24 <filippo.sm.matoso@gmail.com>
2015-05-27 10:38:20 +0100

    added associations to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,9 @@
 class Story < ActiveRecord::Base
+  # ASSOCIATIONS
+  has_and_belong_to_many :images
+  belongs_to :user
 
+  # VALIDATIONS
   validates :title, presence: true, length: { maximum: 25 }
   validates :description, presence: true, length: { maximum: 300 }
 end

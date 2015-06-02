d9d5e570bface912213f0d6aa66a5796e750f3a0
pipo24 <filippo.sm.matoso@gmail.com>
2015-06-02 15:44:49 +0100

    removed unecessary file

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,7 +10,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user.ensure_authentication_token
         cookies[:authentication_token] = @user.authentication_token
         sign_in_and_redirect @user, event: :authentication
-        # set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
       else
         # session["devise.#{provider}_data"] = env["omniauth.auth"]
         redirect_to new_user_registration_url

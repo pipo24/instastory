bd60a384d7e034a5d2f1b80cfd49aee3bbbfff49
pipo24 <filippo.sm.matoso@gmail.com>
2015-06-04 17:19:52 +0100

    changed story create (images find) method

--- lib/instagram_search.rb
+++ lib/instagram_search.rb
@@ -11,8 +11,11 @@ class InstagramSearch
         latitude      = image.location.latitude.to_i
         location_name = image.location.name
       end
+      if image.caption.text?
+        title = image.caption.text.split('#')[0]
+      end
       story.images.create!({
-        title:          image.caption.text.split('#')[0], 
+        title:          title, 
         tags:           image.tags.join(", #").insert(0, "#"), 
         image_username: image.caption.from.full_name, 
         longitude:      longitude, 
@@ -24,25 +27,28 @@ class InstagramSearch
         })
     end
 
-      next_images = client.tag_recent_media(hashtag, {max_id: images.pagination.next_max_id})
-      next_images.each do |image|
-        if image.location?
-          longitude     = image.location.longitude.to_i
-          latitude      = image.location.latitude.to_i
-          location_name = image.location.name
-        end
-        story.images.create!({
-          title:          image.caption.text.split('#')[0], 
-          tags:           image.tags.join(", #").insert(0, "#"), 
-          image_username: image.caption.from.full_name, 
-          longitude:      longitude, 
-          latitude:       latitude, 
-          location_name:  location_name, 
-          uploaded_at:    Time.at(image.created_time.to_i).to_date.to_s,
-          picture:        image.images.standard_resolution.url, 
-          story_id:       story.id
-          })
+    next_images = client.tag_recent_media(hashtag, {max_id: images.pagination.next_max_id})
+    next_images.each do |image|
+      if image.location?
+        longitude     = image.location.longitude.to_i
+        latitude      = image.location.latitude.to_i
+        location_name = image.location.name
       end
+      if image.caption.text?
+        title = image.caption.text.split('#')[0]
+      end
+      story.images.create!({
+        title:          title, 
+        tags:           image.tags.join(", #").insert(0, "#"), 
+        image_username: image.caption.from.full_name, 
+        longitude:      longitude, 
+        latitude:       latitude, 
+        location_name:  location_name, 
+        uploaded_at:    Time.at(image.created_time.to_i).to_date.to_s,
+        picture:        image.images.standard_resolution.url, 
+        story_id:       story.id
+        })
+    end
 
       # next_images = client.tag_recent_media(hashtag, {max_id: images.pagination.next_max_id})
       # next_images.each do |image|
@@ -63,5 +69,5 @@ class InstagramSearch
       #     story_id:       story.id
       #     })
       # end
+    end
   end
-end
